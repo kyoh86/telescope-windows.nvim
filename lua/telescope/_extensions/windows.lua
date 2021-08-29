a30be00535217567d2485e85e74118bb601dd50e
Kyoichiro Yamada <me@kyoh86.dev>
2021-08-29 10:59:38 +0900

    fix

--- lua/telescope/_extensions/windows.lua
+++ lua/telescope/_extensions/windows.lua
@@ -1,10 +1,10 @@
-local actions = require'telescope.actions'
-local conf = require'telescope.config'.values
-local finders = require'telescope.finders'
-local path = require('telescope.path')
-local pickers = require'telescope.pickers'
-local entry_display = require('telescope.pickers.entry_display')
-local utils = require('telescope.utils')
+local actions = require "telescope.actions"
+local conf = require "telescope.config".values
+local finders = require "telescope.finders"
+local Path = require("plenary.path")
+local pickers = require "telescope.pickers"
+local entry_display = require("telescope.pickers.entry_display")
+local utils = require("telescope.utils")
 
 local function show_entry_window(prompt_bufnr)
   local entry = actions.get_selected_entry(prompt_bufnr)
@@ -22,19 +22,20 @@ function M.gen_from_window(opts)
 
   local icon_width = 0
   if not disable_devicons then
-    local icon, _ = utils.get_devicons('fname', disable_devicons)
+    local icon, _ = utils.get_devicons("fname", disable_devicons)
     icon_width = utils.strdisplaywidth(icon)
   end
 
-  local displayer = entry_display.create {
+  local displayer =
+    entry_display.create {
     separator = " ",
     items = {
-      { width = opts.tabpage_width },
-      { width = opts.winnr_width },
-      { width = 4 },
-      { width = icon_width },
-      { remaining = true },
-    },
+      {width = opts.tabpage_width},
+      {width = opts.winnr_width},
+      {width = 4},
+      {width = icon_width},
+      {remaining = true}
+    }
   }
 
   local cwd = vim.fn.expand(opts.cwd or vim.fn.getcwd())
@@ -42,7 +43,7 @@ function M.gen_from_window(opts)
   local make_display = function(entry)
     local display_bufname
     if opts.shorten_path then
-      display_bufname = path.shorten(entry.filename)
+      display_bufname = Path:new({entry.filename}):shorten()
     else
       display_bufname = entry.filename
     end
@@ -53,52 +54,55 @@ function M.gen_from_window(opts)
       {entry.tabpage, "TelescopeResultsNumber"},
       {entry.winnr, "TelescopeResultsNumber"},
       {entry.indicator, "TelescopeResultsComment"},
-      { icon, hl_group },
+      {icon, hl_group},
       display_bufname .. ":" .. entry.lnum
-      }
+    }
   end
 
   return function(entry)
-    local bufname = entry.info.name ~= "" and entry.info.name or '[No Name]'
+    local bufname = entry.info.name ~= "" and entry.info.name or "[No Name]"
     -- if bufname is inside the cwd, trim that part of the string
-    bufname = path.normalize(bufname, cwd)
+    bufname = Path:new({bufname}).normalize(cwd)
 
-    local hidden = entry.info.hidden == 1 and 'h' or 'a'
-    local readonly = vim.api.nvim_buf_get_option(entry.bufnr, 'readonly') and '=' or ' '
-    local changed = entry.info.changed == 1 and '+' or ' '
+    local hidden = entry.info.hidden == 1 and "h" or "a"
+    local readonly = vim.api.nvim_buf_get_option(entry.bufnr, "readonly") and "=" or " "
+    local changed = entry.info.changed == 1 and "+" or " "
     local indicator = entry.flag .. hidden .. readonly .. changed
 
     return {
       valid = true,
-
       value = bufname,
       ordinal = entry.winnr .. " : " .. bufname,
       display = make_display,
-
       tabpage = entry.tabpage,
       winnr = entry.winnr,
       bufnr = entry.bufnr,
       filename = bufname,
-
       lnum = entry.info.lnum ~= 0 and entry.info.lnum or 1,
-      indicator = indicator,
+      indicator = indicator
     }
   end
 end
 
 M.windows = function(opts)
-  local winnrs = vim.tbl_filter(function(w)
-    local win_conf = vim.api.nvim_win_get_config(w)
-    if win_conf.relative ~= '' and not win_conf.focusable then
-      return false
-    end
-    if opts.ignore_current_window and w == vim.api.nvim_get_current_win() then
-      return false
-    end
-    return true
-  end, vim.api.nvim_list_wins())
+  local winnrs =
+    vim.tbl_filter(
+    function(w)
+      local win_conf = vim.api.nvim_win_get_config(w)
+      if win_conf.relative ~= "" and not win_conf.focusable then
+        return false
+      end
+      if opts.ignore_current_window and w == vim.api.nvim_get_current_win() then
+        return false
+      end
+      return true
+    end,
+    vim.api.nvim_list_wins()
+  )
 
-  if not next(winnrs) then return end
+  if not next(winnrs) then
+    return
+  end
 
   local windows = {}
   local default_selection_idx = 1
@@ -106,7 +110,7 @@ M.windows = function(opts)
   local max_tabpage = 0
   local max_winnr = 0
   for _, winnr in ipairs(winnrs) do
-    local flag = winnr == vim.api.nvim_get_current_win() and '%' or ' '
+    local flag = winnr == vim.api.nvim_get_current_win() and "%" or " "
 
     local tabpage = vim.api.nvim_win_get_tabpage(winnr)
     local bufnr = vim.fn.winbufnr(winnr)
@@ -115,7 +119,7 @@ M.windows = function(opts)
       tabpage = tabpage,
       bufnr = bufnr,
       flag = flag,
-      info = vim.fn.getbufinfo(bufnr)[1],
+      info = vim.fn.getbufinfo(bufnr)[1]
     }
 
     if max_tabpage < tabpage then
@@ -141,24 +145,27 @@ M.windows = function(opts)
     opts.winnr_width = #tostring(max_winnr)
   end
 
-  pickers.new(opts, {
-    prompt_title = 'Windows',
-    finder    = finders.new_table {
-      results = windows,
-      entry_maker = opts.entry_maker or M.gen_from_window(opts)
-    },
-    previewer = conf.grep_previewer(opts),
-    sorter = conf.generic_sorter(opts),
-    default_selection_index = default_selection_idx,
-    attach_mappings = function(_)
-      actions.select_default:replace(show_entry_window)
-      return true
-    end,
-  }):find()
+  pickers.new(
+    opts,
+    {
+      prompt_title = "Windows",
+      finder = finders.new_table {
+        results = windows,
+        entry_maker = opts.entry_maker or M.gen_from_window(opts)
+      },
+      previewer = conf.grep_previewer(opts),
+      sorter = conf.generic_sorter(opts),
+      default_selection_index = default_selection_idx,
+      attach_mappings = function(_)
+        actions.select_default:replace(show_entry_window)
+        return true
+      end
+    }
+  ):find()
 end
 
-return require('telescope').register_extension{
+return require("telescope").register_extension {
   exports = {
-    windows = M.windows,
-  },
+    windows = M.windows
+  }
 }
